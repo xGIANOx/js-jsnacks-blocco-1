0515d6f903557f70fb8449830f5cc0d6b18615b9
xGIANOx <studioenricogian@gmail.com>
2023-03-15 11:50:05 +0100

    [add] while loop

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,4 +1,4 @@
-let total = 0;
+/*let total = 0;
 
 for (let i = 1; i <= 10; i++) {
   let number = parseInt(prompt("Inserisci il " + i + "° numero:"));
@@ -6,4 +6,21 @@ for (let i = 1; i <= 10; i++) {
 }
 
 console.log("La somma di tutti i numeri inseriti è: " + total);
+*/
 
+// con while loop
+
+
+let total = 0;
+
+let j = 0
+
+while (j <  10) {
+  
+  let number = parseInt(prompt("Inserisci il " + j + "° numero:"));
+  total += number;
+  
+  j++
+}
+
+console.log("La somma di tutti i numeri inseriti è: " + total);
